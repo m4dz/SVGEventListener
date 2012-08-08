3b6f138ccadd7e190c468c065eac34e45ed242b7
MAD <ecrire@madsgraphics.com>
2012-08-08 08:10:49 +0200

    Add queue animation support

--- SVGEventListener.js
+++ SVGEventListener.js
@@ -11,12 +11,12 @@
 //
 // Please minify before use.
 //
-( function ( window, doc, element ) {
+( function ( window, doc, el ) {
 
   'use strict';
 
-  var addEventListener_legacy   = element.prototype.addEventListener,
-      svg                       = doc.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ),
+  var addEventListenerLegacy   = el.prototype.addEventListener,
+      svg                      = doc.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ),
       // helper functions
       isString                  = function ( s ) {
         return typeof s == "string";
@@ -79,7 +79,7 @@
         time += times[t]*Math.pow(60, t)*1000;
       }
 
-      return Number( time );
+      return time;
     }
   }
 
@@ -95,11 +95,15 @@
   // Create custom listener object with private property to store listeners
   function EventListener() {
     this._listeners = {};
+    this.autoFire   = {
+      begin: false,
+      end: false
+    };
   }
 
   // Extends it to add and fire events
   EventListener.prototype = {
-    constructor : EventListener,
+    constructor: EventListener,
     // add new event to listeners
     add: function addListener( type, listener ) {
       // if there is no triggers already defined for this events,
@@ -118,6 +122,7 @@
       }
       // set target if unavailable
       if ( !event.target ) {
+        // FIXME: Fix correct target for event
         event.target = this;
       }
       // if there is no event given, throw an error
@@ -137,22 +142,69 @@
   // Overwrite Element.addEventListener method for transparency fallback
   //
   // Inpired by: http://stackoverflow.com/questions/7220515/extending-node-addeventlistener-method-with-the-same-name#7220628
-  element.prototype.addEventListener = function( type, listener, useCapture ) {
+  el.prototype.addEventListener = function ( type, listener, useCapture ) {
     var timeout,
-    // Attach events stack to element
-        listeners = this.listeners = new EventListener();
-    /** CASE 1 : endEvent **/
-    // check event name and support for the event
-    if ( type == 'endEvent' && !isEventSupported(type.substring(0, type.indexOf('Event'))) ) {
-      // register listener to the events
-      listeners.add( type, listener );
-      // set a timer based on begin and dur attributes
-      timeout = clocker( this.getAttribute('begin') ) + clocker( this.getAttribute('dur') );
-      window.setTimeout( function() { listeners.fire(type); }, timeout );
+        index,
+        begin    = this.getAttribute('begin'),
+        duration = this.getAttribute('dur'),
+        that     = this;
+    // ***
+    // Attach a new event listeners stack if it doesn't exists
+    if( isUndefined( this.listeners ) )
+    {
+      this.listeners = new EventListener();
     }
-
+    // ***
+    // check event name and support for endEvent
+    if ( type == 'endEvent' && !isEventSupported( 'end' ) ) {
+      // Add listener to the endEvent stack
+      this.listeners.add( type, listener );
+      // check if autofire is already enabled
+      if ( !this.listeners.autoFire.end ) {
+        // if not, add an autofired at the end of animation (=dur)
+        this.addEventListener ( 'beginEvent' , function () {
+          window.setTimeout( function () {
+            that.listeners.fire( 'endEvent' );
+          }, clocker( duration ) )
+        });
+        // and set the autofire flag at true to prevent multiple endfire
+        // launch
+        this.listeners.autoFire.end = true;
+      }
+    }
+    // ***
+    // check event name and suport for beginEvent
+    if ( type == 'beginEvent' && !isEventSupported( 'begin' ) ) {
+      // Add listener to the endEvent stack
+      this.listeners.add( type, listener );
+      // Check if begin is set to a duration
+      if ( begin != 'indefinite' && begin.indexOf( '.end' ) == -1 ) {
+        // true, so check the autofire begin event
+        if ( !this.listeners.autoFire.begin ) {
+          // not already activated, so activate it
+          window.setTimeout( function () {
+            that.listeners.fire( 'beginEvent' );
+          }, clocker( begin ) );
+          // set autofire to true to prevent multiple launch
+          this.listeners.autoFire.begin = true;
+        }
+      }
+      // if the lanch depends of the end of another animation
+      else if ( (index = begin.indexOf( '.end' )) != -1 ) {
+        var previousAnimate = doc.getElementById( begin.substr(0, index) );
+        // Add an endEvent that launch the next animation
+        previousAnimate.addEventListener( 'endEvent', function () {
+          that.listeners.fire( 'beginEvent' );
+        });
+      }
+      // if the launch depends of a manual action (=indefinite)
+      else if ( begin == 'indefinite') {
+        // TODO: What to do in this case ?
+      }
+    }
+    // ***
     // call the original method for fallback
-    return addEventListener_legacy.call( this, type, listener, useCapture );
+    return addEventListenerLegacy.call( this, type, listener, useCapture );
   };
 
 })( this, document, Element );
